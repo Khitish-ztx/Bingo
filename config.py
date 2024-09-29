b89ee67de43889c3a3647f6f83e137c53c6895db
Khitish-ztx <khitish152006@gmail.com>
2024-09-29 15:52:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-## What's up Kangers
-## Don't Kang without Creadits else I will rape your mom
-
 import os
 from os import getenv
 from dotenv import load_dotenv
@@ -11,54 +8,41 @@ if os.path.exists("local.env"):
 load_dotenv()
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME")
+# Filled variables
+SESSION_NAME = "MyBotSession"  # You can change this to any name you prefer
 
-if str(getenv("STRING_SESSION2")).strip() == "":
-    SESSION2 = str(None)
-else:
-    SESSION2 = str(getenv("STRING_SESSION2"))
+SESSION2 = "BQFToMkAKe5Ozfqc7R_Y8p0zQb6ShWVVhZkxtahYFE73F-UisrR-mMGoj5_aUNgTWkW-ueDdlyRLUJ2qBEjWm1OiNwES9t31CaAl-z4Tu6SzeSxRZeeAw4fu3X6mu8J74RKAJtFrCi_rh3zwwYR_z78DzzFlZmZC4G3COgg-3frD8y6JSSwD5Gq-zEPh0nUEVAzt-eoa7uaPi5QR78D_bQd6ok8EhqY3DQHcmIab23Zaf8GWGtv-Td4j1fvmMheM2iw5YasULrV3jXxzfiYVGbkhxj2FwjL0Ju272FonaOnHaP2ZE7nPBRP19XdO9HvmBSmSUdg6Jka7lC_3CtY_-JFB-A0tIQAAAABiNGsPAA"
 
-if str(getenv("STRING_SESSION3")).strip() == "":
-    SESSION3 = str(None)
-else:
-    SESSION3 = str(getenv("STRING_SESSION3"))
+SESSION3 = None
+SESSION4 = None
+SESSION5 = None
 
-if str(getenv("STRING_SESSION4")).strip() == "":
-    SESSION4 = str(None)
-else:
-    SESSION4 = str(getenv("STRING_SESSION4"))
+BOT_TOKEN = "6327930809:AAEWwQaVMw_a0pxotKkM8C2BmBFaF3jLW6M"
+BOT_NAME = "Iᴛᴀᴄʜɪ ᴍᴜsɪᴄ ʙᴏᴛ"  # Updated bot name
 
-if str(getenv("STRING_SESSION5")).strip() == "":
-    SESSION5 = str(None)
-else:
-    SESSION5 = str(getenv("STRING_SESSION5"))
-
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_NAME = getenv("BOT_NAME", "Umk")
-
-API_ID = int(getenv("API_ID", "8186557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
-MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Cloner:Cloner@cluster0.cgc6t.mongodb.net/?retryWrites=true&w=majority")
-OWNER_NAME = getenv("OWNER_NAME", "Zaid")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Timesisnotwaiting")
-ALIVE_NAME = getenv("ALIVE_NAME", "Zaid")
-BOT_USERNAME = getenv("BOT_USERNAME", "Zaid2_Robot")
-OWNER_ID = getenv("OWNER_ID", "1669178360")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Zaid2_Assistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "TheSupportChat")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TheUpdatesChannel")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "HEROKU_API_KEY")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/fc9d87ffd1c6f828eb7fc.png")
-START_PIC = getenv("START_PIC", "https://telegra.ph/file/a414e2cdfeaa7d4414b89.png")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ITZ-ZAID/Zaid-Vc-Player")
-PLAY_IMG = getenv("PLAY_IMG", "https://telegra.ph/file/10b1f781170b1e1867f68.png")
-QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
-CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-VIDEO_IMG = getenv("VIDEO_IMG", "https://telegra.ph/file/6213d2673486beca02967.png")
-SKIP_IMG = getenv("SKIP_IMG", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
-NEXT_IMG = getenv("NEXT_IMG", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
-HEROKU_MODE = getenv("HEROKU_MODE", None)
+API_ID = 22257865
+API_HASH = "97fc23c20e06f411abbf36cad8e118ce"
+MONGO_DB_URL = "mongodb+srv://Cloner:Cloner@cluster0.cgc6t.mongodb.net/?retryWrites=true&w=majority"
+OWNER_NAME = "itachi uchiha"
+OWNER_USERNAME = "itachi_ztx"
+ALIVE_NAME = "Zaid"
+BOT_USERNAME = "@Itachising_bot"  # Updated bot username
+OWNER_ID = 1647602447  # Updated owner ID
+ASSISTANT_NAME = "Zaid2_Assistant"
+GROUP_SUPPORT = "TheSupportChat"
+UPDATES_CHANNEL = "TheUpdatesChannel"
+HEROKU_APP_NAME = None  # Fill in if you have it
+HEROKU_API_KEY = None  # Fill in if you have it
+SUDO_USERS = [1647602447]  # Only your user ID as the Sudo user
+COMMAND_PREFIXES = ["/", "!", "."]  # List of command prefixes
+ALIVE_IMG = "https://telegra.ph/file/fc9d87ffd1c6f828eb7fc.png"
+START_PIC = "https://telegra.ph/file/a414e2cdfeaa7d4414b89.png"
+DURATION_LIMIT = 60  # Duration in minutes
+UPSTREAM_REPO = "https://github.com/ITZ-ZAID/Zaid-Vc-Player"
+PLAY_IMG = "https://telegra.ph/file/10b1f781170b1e1867f68.png"
+QUE_IMG = "https://telegra.ph/file/b95c13eef1ebd14dbb458.png"
+CMD_IMG = "https://telegra.ph/file/66518ed54301654f0b126.png"
+VIDEO_IMG = "https://telegra.ph/file/6213d2673486beca02967.png"
+SKIP_IMG = "https://telegra.ph/file/f02efde766160d3ff52d6.png"
+NEXT_IMG = "https://telegra.ph/file/f02efde766160d3ff52d6.png"
+HEROKU_MODE = None  # Fill in if you have it
